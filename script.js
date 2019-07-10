ef605d69db73bfbb0ed698ae9559e74ce90eab13
Masatoshi Makimoto <masa526sk8@gmail.com>
2019-07-10 22:35:31 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+{
+  const start_btn = document.getElementById('start');
+  const stop_btn = document.getElementById('stop');
+  const clear_btn = document.getElementById('clear');
+  const speech = new webkitSpeechRecognition();
+
+  const box = document.getElementById('box');
+  
+  speech.lang = "ja-JP";
+  start_btn.addEventListener('click', () => {
+    start_btn.disabled = true;
+    stop_btn.disabled = false;
+    speech.start();
+  });
+  stop_btn.addEventListener('click', () => {
+    start_btn.disabled = false;
+    stop_btn.disabled = true;
+    start_btn.textContent = 'Start';
+    speech.stop();
+  });
+  clear_btn.addEventListener('click', () => {
+    box.innerHTML = "";
+  });
+  
+  speech.onresult = (e) => {
+    speech.stop();
+    if (e.results[0].isFinal) {
+      let div = document.createElement('div');
+      div.textContent = e.results[0][0].transcript;
+      box.appendChild(div);
+    }    
+  };
+  speech.onend = (e) => {
+    if (start_btn.disabled) {
+      speech.start();
+    }
+  }
+  speech.onsoundstart = () => {
+    start_btn.textContent = 'Processing...';
+  }
+  speech.onsoundend = () => {
+    start_btn.textContent = 'Waiting...';
+  }
+}
